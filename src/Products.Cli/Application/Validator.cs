ed9a91484f182c9c21cc46be6c65e8ed6daadbd3
Amin Chouaibi el Azaar <achouaibi@plainconcepts.com>
2022-03-18 11:37:06 +0100

    Adding more rules on validator.

--- src/Products.Cli/Application/Validator.cs
+++ src/Products.Cli/Application/Validator.cs
@@ -1,16 +1,38 @@
 ﻿namespace Products.Cli.Application;
 
 using FluentValidation;
-using Products.Cli.Application.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Products.Cli.Application.Utils;
 
 public class CommandValidator : AbstractValidator<Command>
 {
-    private List<string> _availableDataSources = new List<string> { "CAPTERRA", "SOFTWAREADVICE" };
     public CommandValidator()
     {
         RuleFor(_ => _.InputData).NotEmpty();
+        RuleFor(_ => _.InputData).Must(x => IsValidJson(x))
+                                 .When(x => x.DataSourceName == Constants.CAPTERRA_NAME);
         RuleFor(_ => _.DataSourceName).NotEmpty();
-        RuleFor(_ => _.DataSourceName).Must(x => _availableDataSources.Contains(x))
+        RuleFor(_ => _.DataSourceName).Must(x => Constants.AVAILABLE_DATA_SOURCES.Contains(x))
                                       .WithMessage("Unavailable data source");
+    }
+
+    private bool IsValidJson(string strInput)
+    {
+        strInput = strInput.Trim();
+
+        if (!(strInput.StartsWith("{") && strInput.EndsWith("}")) && !(strInput.StartsWith("[") && strInput.EndsWith("]")))
+            return false;
+        
+        try
+        {
+            var obj = JToken.Parse(strInput);
+            return true;
+        }
+        catch (JsonReaderException jex)
+        {
+            Console.WriteLine(jex.Message);
+            return false;
+        }
     }
 }
