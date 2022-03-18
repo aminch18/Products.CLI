6ba76de99f0735be6077b5d75da5d4e2e0052ca9
Amin Chouaibi el Azaar <achouaibi@plainconcepts.com>
2022-03-18 11:28:11 +0100

    Adding class to centralize constant values.

---
+++ src/Products.Cli/Application/Utils/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Products.Cli.Application.Utils;
+
+public class Constants
+{
+    public static string CAPTERRA_NAME = "CAPTERRA";
+    public static string SFTW_ADVICE_NAME = "SOFTWAREADVICE";
+    public static List<string> AVAILABLE_DATA_SOURCES = new List<string> { CAPTERRA_NAME, SFTW_ADVICE_NAME };
+}
