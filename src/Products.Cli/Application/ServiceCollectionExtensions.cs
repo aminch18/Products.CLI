2428d9322e6509e0fd28bcfd5213040817dfec9d
Amin Chouaibi el Azaar <achouaibi@plainconcepts.com>
2022-03-18 11:38:26 +0100

    Restructuring program.cs  and creating an extension method to wrap application services registration.

---
+++ src/Products.Cli/Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Products.Cli.Application;
+
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Products.Cli.Application.Abstractions;
+using Products.Cli.Application.Services;
+using Products.Cli.Services.Serializers;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+public static class ServiceCollectionExtensions
+{
+    private static IDeserializer CreateYamlDeserializer() => new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance)
+                                                                   .Build();
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        => services.AddSingleton<IDeserializer>(CreateYamlDeserializer())
+                               .AddSingleton<IYmlSerializer, YmlSerializer>()
+                               .AddSingleton<IJSONSerializer, JSONSerializer>()
+                               .AddSingleton<IValidator<Command>, CommandValidator>()
+                               .AddSingleton<IImportDataService, ImportDataService>()
+                               .AddScoped<IHandler<Command>, Handler>()
+                               .AddScoped<IMainManager, MainManager>();
+}
+
--- src/Products.Cli/Program.cs
+++ src/Products.Cli/Program.cs
@@ -1,28 +1,11 @@
-﻿using FluentValidation;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Products.Cli;
 using Products.Cli.Application;
-using Products.Cli.Application.Abstractions;
-using Products.Cli.Application.Models;
-using Products.Cli.Application.Services;
-using Products.Cli.Application.Utils;
-using Products.Cli.Services;
-using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
+using Microsoft.Extensions.DependencyInjection;
 
-#region Registering services on service collection and building service provider.
-
-IDeserializer CreateYamlDeserializer() => new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance)
-                                                                   .Build();
-var services = new ServiceCollection();
-var servicesProvider = services.AddSingleton<IDeserializer>(CreateYamlDeserializer())
-                               .AddSingleton<IYmlSerializer, YmlSerializer>()
-                               .AddSingleton<IJSONSerializer, JSONSerializer>()
-                               .AddSingleton<IValidator<Command>, CommandValidator>()
-                               .AddSingleton<IImportDataService, ImportDataService>()
-                               .AddScoped<IHandler<Command>, Handler>()
-                               .AddScoped<IMainManager, MainManager>()
+var servicesProvider = new ServiceCollection()
+                               .AddApplicationServices()
                                .BuildServiceProvider();
-#endregion
+
 var arguments = Environment.GetCommandLineArgs();
 await servicesProvider.GetService<IMainManager>()
                       .ExecuteAsync(arguments[1], arguments[2]);
